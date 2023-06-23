b12654da7f95b0638707cdfd112e01758e772a23
samuel de jesus <samue@MacBook-Air.local>
2023-06-22 21:28:27 -0400

    debug1

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const path = require("path")
+require('dotenv').config( '__dirname/.env');
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    'techblog_db',
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+
+module.exports = sequelize;
